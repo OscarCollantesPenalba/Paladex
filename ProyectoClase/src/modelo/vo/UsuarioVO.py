418b5e85c3083998c9a4a521186b733c1382dd4c
GrefuriusRL <riglev01@estudiantes.unileon.es>
2026-05-05 14:41:46 +0200

    Delete ProyectoClase directory

--- ProyectoClase/src/modelo/vo/UsuarioVO.py
+++
@@ -1,8 +0,0 @@
-class UsuarioVO:
-    
-    def __init__(self, id_usuario=None, nombre=None, correo=None, contrasena=None,id_rol = None):
-        self.id_usuario = id_usuario
-        self.nombre = nombre
-        self.correo = correo
-        self.contrasena = contrasena
-        self.id_rol = id_rol
